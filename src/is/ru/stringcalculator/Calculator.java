7e7e3b3123481d0d6fecd38b90309a499c4207cf
einkria <kristintj15@ru.is>
2016-10-14 21:27:28 +0200

    First step finished - three tests pass

--- src/is/ru/stringcalculator/Calculator.java
+++ src/is/ru/stringcalculator/Calculator.java
@@ -3,6 +3,18 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		return 0;
+		if(text.isEmpty()){
+			return 0;
+		}
+		else if( text.contains(",")){
+			String [] sa = text.split(",");
+			return toInt(sa[0]) + toInt(sa[1]);
+		}
+		else
+			return 1;
+	}
+	
+	private static int toInt(String text){
+		return Integer.parseInt(text);
 	}
 }
